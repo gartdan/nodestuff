e54caf4f4e8bf5ab8da8103938c8e717aa31d176
Dan Gartner <dgartner@gmail.com>
2014-03-31 12:26:06 -0500

    added todo sample

--- http/http/app.js
+++ http/http/app.js
@@ -2,19 +2,25 @@ var http = require('http');
 var parse = require('url').parse;
 var join = require('path').join;
 var fs = require('fs');
+var qs = require('querystring');
 
+var items = [];
 var root = __dirname;
 var server = http.createServer(function(req, res) {
     var url = parse(req.url);
-    var path = join(root, url.pathname);
-    fs.stat(path, function(err, stat) {
-        if(err) {
-            if(err.code == 'ENOENT') notFound(req, res);
-            else serverError(req, res);
-        } else {
-            sendFile(req, res, stat, path);
-        }
-    });
+    if(url.path == '/todo') {
+        todo(req, res);
+    } else {
+        var path = join(root, url.pathname);
+        fs.stat(path, function(err, stat) {
+            if(err) {
+                if(err.code == 'ENOENT') notFound(req, res);
+                else serverError(req, res);
+            } else {
+                sendFile(req, res, stat, path);
+            }
+        });
+    }
 });
 
 function sendFile(req, res, stat, path) {
@@ -23,6 +29,39 @@ function sendFile(req, res, stat, path) {
     stream.pipe(res);
     stream.on('error', function(err) {
         serverError(req, res);
+    });
+}
+
+function todo(req, res) {
+    if(req.method == 'GET')
+        todoGet(req, res);
+    else if (req.method == 'POST')
+        todoPost(req, res);
+}
+
+function todoGet(req, res) {
+    var html = '<html><head><title>Todo</title></head>'
+    + '<body><h1>Todo List</h1><div>'
+    + '<ul>'
+    + items.map(function(item) { return '<li>'+item+'</l1>'; }).join(' ')
+    + '</ul>'
+    + '<form method="post" action="/todo">'
+    + '<p><input type="text" name="item" /></p>'
+    + '<p><input type="submit" value="Add" /></p>'
+    + '</form></body></html>';
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(html));
+    res.end(html);
+}
+
+function todoPost(req, res) {
+    var body = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) { body += chunk; });
+    req.on('end', function() {
+        var obj = qs.parse(body);
+        items.push(obj.item);
+        todoGet(req, res);
     });
 }
 
